5eb0fc1444c3679a4169f5838e188991acd662aa
e.atayi <erfan.at799@gmail.com>
2023-02-04 17:16:38 +0330

    start migrate project from js to TS

--- connection/redis.connection.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-const appConfig = require('../config/application');
-const Redis = require("ioredis");
-const redisClient = new Redis(appConfig.Redis);
-console.log('======> appConfig.redis: ', JSON.stringify(appConfig.redis));
-
-module.exports = { redisClient }
